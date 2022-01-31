f1756693716624a2924beb2ccb93a8382d7a5b57
patriciaderieux <6089242+patty345@users.noreply.github.com>
2022-01-30 19:12:58 -0600

    added error handling

--- server.js
+++ server.js
@@ -41,13 +41,10 @@ function filterByQuery(query, animalsArray) {
       return filteredResults;
 }
 
-
-
-
-
-
-
-
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
 
 
 
@@ -61,6 +58,16 @@ app.get('/api/animals', (req, res) => {
     }
     res.json(results);
 });
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+       res.json(result);
+    } else {
+        res.send(404);
+    }
+      
+  });
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
