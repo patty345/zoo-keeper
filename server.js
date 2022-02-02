25062836237cbb8a8051255ba81985eed23d01a7
patriciaderieux <6089242+patty345@users.noreply.github.com>
2022-02-01 21:12:32 -0600

    added front end

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.static('public'));
+
 app.use(express.urlencoded({ extended: true}));
 
 app.use(express.json());
@@ -110,6 +112,22 @@ app.post('/api/animals', (req, res) => {
     } 
 });  
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+  });
+
+  app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+  });
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+  
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
